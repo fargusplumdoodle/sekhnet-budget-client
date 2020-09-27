c1e023f8ae6f66c4884482e718580042b3b62385
fargus plumdoodle <fargusplumdoodle@gmail.com>
2020-09-26 20:11:09 -0700

    added pie chart

--- lib/ui/widgets/dashboard.dart
+++ lib/ui/widgets/dashboard.dart
@@ -1,6 +1,7 @@
 import 'package:budget/globals.dart';
 import 'package:budget/model/budget.dart';
 import 'package:budget/ui/screens/budget_detail.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:flutter/material.dart';
 
 class BudgetOverview extends StatelessWidget {
@@ -18,7 +19,7 @@ class BudgetOverview extends StatelessWidget {
             scrollDirection: Axis.horizontal,
             children: [
               BudgetList(budgets),
-              BudgetList(budgets),
+              BudgetPie(budgets),
             ],
           ),
         ),
@@ -96,4 +97,58 @@ Widget BudgetStatusCard(List<BudgetModel> budgets) {
       ))
     ],
   ));
+}
+
+class BudgetPie extends StatelessWidget {
+  final List<BudgetModel> budgets;
+  var maxBudget;
+
+  BudgetPie(this.budgets);
+
+  @override
+  Widget build(BuildContext context) {
+    maxBudget = getMaxBudget();
+    return charts.PieChart(_generatePieData(),
+        animate: true,
+        defaultRenderer: new charts.ArcRendererConfig(arcRendererDecorators: [
+          new charts.ArcLabelDecorator(
+              labelPosition: charts.ArcLabelPosition.inside)
+        ]));
+  }
+
+  BudgetModel getMaxBudget() {
+    var maxBudget = budgets[0];
+    for (var i = 0; i < budgets.length; i++) {
+      if (budgets[i].balance > maxBudget.balance) {
+        maxBudget = budgets[i];
+      }
+    }
+    return maxBudget;
+  }
+
+  List<charts.Series<BudgetModel, String>> _generatePieData() {
+    return [
+      new charts.Series(
+        id: 'Budget Balance Distribution',
+        data: this.budgets,
+        domainFn: (BudgetModel budget, _) => budget.name,
+        measureFn: (BudgetModel budget, _) => budget.balance,
+        labelAccessorFn: (BudgetModel budget, _) => "${budget.name}",
+        colorFn: (BudgetModel budget, __) => getColor(budget),
+      ),
+    ];
+  }
+
+  charts.Color getColor(BudgetModel budget) {
+    return charts.MaterialPalette.teal.shadeDefault;
+    // TODO: dynamic colors
+    if (budget.name == maxBudget.name) {
+      return charts.MaterialPalette.teal.shadeDefault;
+    }
+    var shades = charts.MaterialPalette.teal.makeShades(100);
+    var index = 100 -
+        ((budget.balance.toDouble() / maxBudget.balance.toDouble()) * 100)
+            .round();
+    return shades[index];
+  }
 }
